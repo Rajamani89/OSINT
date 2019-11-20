f30782bef073432c47f3de0b0f36da94ec79433a
Rajamani <40541154+Rajamani89@users.noreply.github.com>
2019-11-20 08:05:52 +0530

    Update and rename downloading nvdzip file.py to 1.2 - downloading nvdzip file.py

--- 1.2 - downloading nvdzip file.py
+++ 1.2 - downloading nvdzip file.py
@@ -1,50 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import requests
 import os
 import zipfile
 import csv
 import json
+import io
+# Downloading the zip file
 page = "https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-2019.json.zip"
 result = requests.get(page)
-
-
-# In[8]:
-
-
-import io
+# extracting the zipfile
 zipout = zipfile.ZipFile(io.BytesIO(result.content))
-
-
-# In[9]:
-
-
 zipout.extractall()
-
-
-# In[30]:
-
-
+# opening JSON file
 nvdjson = open("nvdcve-1.1-2019.json")
-
-
-# In[31]:
-
-
 nvddict = json.load(nvdjson)
-
-
-# In[40]:
-
-
+# WORK in progress you have to iterate here for CVE items
 nvddict['CVE_Items'][0]
 
-
-# In[ ]:
 
 
 
