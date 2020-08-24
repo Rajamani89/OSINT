69cbeed4e7fe120e95cff20285352c12e02b357a
Rajamani <40541154+Rajamani89@users.noreply.github.com>
2020-08-24 17:16:33 +0530

    Update 3.3-VT_API_based_whois_extraction_from_virustotal.py

--- 3.3-VT_API_based_whois_extraction_from_virustotal.py
+++ 3.3-VT_API_based_whois_extraction_from_virustotal.py
@@ -1,7 +1,11 @@
 import requests
 import json,csv
 # importing the source data of ipaddress
-ipinfo = ["161.135.105.139","161.135.105.140","204.135.9.10"]
+ipinfo = ["103.18.108.129",
+"109.74.157.200",
+"109.74.157.53",
+"120.150.207.13",
+"139.162.222.202"]
 # next step is forming the url
 vturl = []
 for i in ipinfo:
@@ -9,20 +13,94 @@ for i in ipinfo:
 
 import time
 #to slow down my request
-domaininfowrite = open('ipinfo4.csv','w',newline='')
+domaininfowrite = open('ipinfo5.csv','w',newline='')
 writer = csv.writer(domaininfowrite,lineterminator='\n')
 writer.writerow(['vturl','registrant_country','whois_map','CIDR','NetName','NetRange',])
 # step 4 send all url in as a request
 #writing them during the request itself
 querystring = {"limit":"10"}
-headers = {'x-apikey':'put your key here'}
+headers = {'x-apikey':'enter your key'}
+
+cidr = []
+NetName = []
+NetRange = []
+NetType = []
+OrgTechEmail = []
+OrgTechName = []
+Organization = []
+OrgName = []
+count = 0
 for i in vturl:
   response = requests.request("GET",i, headers=headers, params=querystring)
+  count = count + 1
+  print(count)
   output = json.loads(response.text.encode('utf8'))
-  for y in range(len(output['data'])):
-    writer.writerow([i,output['data'][y]['attributes']['registrant_country'],output['data'][y]['attributes']['whois_map']['Address'],output['data'][y]['attributes']['whois_map']['CIDR'],output['data'][y]['attributes']['whois_map']['NetName'],output['data'][y]['attributes']['whois_map']['NetRange'],output['data'][y]['attributes']['whois_map']['NetType'],output['data'][y]['attributes']['whois_map']['OrgTechEmail'],output['data'][y]['attributes']['whois_map']['OrgTechName'],output['data'][y]['attributes']['whois_map']['Organization'],output['data'][y]['attributes']['whois_map']['OrgName']])
-    time.sleep(4)
+  time.sleep(5)
+  if output['data'] == []:
+    cidr.append("none found")
+    NetName.append("none found")
+    NetRange.append("none found")
+    NetType.append("none found")
+    OrgTechEmail.append("none found")
+    OrgTechName.append("none found")
+    Organization.append("none found")
+    OrgName.append("none found")
+  else:
+    try:
+  	  cidr.append(output['data'][0]['attributes']['whois_map']['CIDR'])
 
+    except KeyError:
+  	  cidr.append("none found")
+  	  print(i,"none found cidr")
+
+    try:
+  	  NetName.append(output['data'][0]['attributes']['whois_map']['NetName'])
+    except KeyError:
+  	  NetName.append("none found")
+  	  print(i,"none found netname")
+
+    try:
+  	  NetRange.append(output['data'][0]['attributes']['whois_map']['NetRange'])
+    except KeyError:  	
+  	  NetRange.append("none found")
+  	  print(i,"none found NetRange")
+
+    try:
+  	  NetType.append(output['data'][0]['attributes']['whois_map']['NetType'])
+    except KeyError:
+  	  NetType.append("none found")
+  	  print(i,"none found NetType")
+
+    try:
+  	  OrgTechEmail.append(output['data'][0]['attributes']['whois_map']['OrgTechEmail'])
+    except KeyError:
+  	  OrgTechEmail.append("none found")
+  	  print(i,"none found orgtechemail")
+
+    try:
+  	  OrgTechName.append(output['data'][0]['attributes']['whois_map']['OrgTechName'])
+    except KeyError:
+  	  OrgTechName.append("none found")
+  	  print(i,"none found OrgTechName")
+
+    try:
+  	  Organization.append(output['data'][0]['attributes']['whois_map']['Organization'])
+    except KeyError:
+  	  Organization.append("none found")
+  	  print(i,"none found Organization")
+
+    try:
+  	  OrgName.append(output['data'][0]['attributes']['whois_map']['OrgName'])
+    except KeyError:
+  	  OrgName.append("none found")
+  	  print(i,"none found OrgName")
+	
+    time.sleep(9)
+	
+  
+  print(i)
+for y in range(len(vturl)):
+	writer.writerow([vturl[y],cidr[y],NetName[y],NetRange[y],OrgTechEmail[y],OrgTechName[y],Organization[y],OrgName[y]])
     
 from google.colab import files
 files.download('/content/ipinfo4.csv')
