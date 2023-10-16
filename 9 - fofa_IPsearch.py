72f1affca8d4b808c54b811038d7ef1ba780498d
Rajamani <40541154+Rajamani89@users.noreply.github.com>
2023-10-16 23:51:45 +0530

    Update 9 - fofa_IPsearch.py

--- 9 - fofa_IPsearch.py
+++ 9 - fofa_IPsearch.py
@@ -12,11 +12,12 @@ ip_addresses = ["72.9.100.68",
 "88.99.242.20"]
 timer = 1
 # Create or open a CSV file to save the results
-with open('fofa_results1.csv', mode='w', newline='') as csv_file:
+with open('fofa_results2.csv', mode='w', newline='') as csv_file:
     writer = csv.writer(csv_file)
 
     # Write the header row
     writer.writerow(['IP', 'URL', 'Port'])
+    num_results = 0
 
     # Iterate over the list of IP addresses
     for ip in ip_addresses:
@@ -27,210 +28,18 @@ with open('fofa_results1.csv', mode='w', newline='') as csv_file:
 
         # Perform the search
         results = client.search(query_str)
-
-        # Get the first result from the list (if available)
-        first_result = results['results'][0] if 'results' in results and len(results['results']) > 0 else []
-
-        # Extract IP, Country, and City information
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][1] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-
-        first_result = results['results'][2] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][3] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][4] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][4] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][5] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-
-        first_result = results['results'][6] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-
-        first_result = results['results'][7] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-
-        first_result = results['results'][8] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-
-        first_result = results['results'][9] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][10] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][11] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-
-        first_result = results['results'][12] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-
-        first_result = results['results'][13] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-
-        first_result = results['results'][14] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][15] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][16] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][17] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][18] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][19] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][20] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][21] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
-
-        first_result = results['results'][22] if 'results' in results and len(results['results']) > 0 else []
-        ip = first_result[0] if len(first_result) > 0 else 'N/A'
-        url = first_result[1] if len(first_result) > 1 else 'N/A'
-        port = first_result[2] if len(first_result) > 2 else 'N/A'
-
-        # Write the data to the CSV file
-        writer.writerow([ip, url, port])
+        num_results = len(results['results'])
+        num_results = num_results - 1
+        if num_results > 25:
+            num_results = 25
+        if num_results > 0:
+            for i in range(0, num_results):
+                first_result = results['results'][i] if 'results' in results and len(results['results']) > 0 else []
+                # Extract IP, Country, and City information
+                ip = first_result[0] if len(first_result) > 0 else 'N/A'
+                url = first_result[1] if len(first_result) > 1 else 'N/A'
+                port = first_result[2] if len(first_result) > 2 else 'N/A'
+                writer.writerow([ip, url, port])
 
         # Add a delay of 30 seconds between queries
         time.sleep(1)
-
